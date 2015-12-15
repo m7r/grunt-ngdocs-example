ca9bd368240a7b696d8fc1909c353d9feaae0130
Igor Raush <igor.rausch@gmail.com>
2015-12-15 14:23:43 -0800

    feat(): upgrade grunt-ngdocs, add controller example

---
+++ src/controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * @ngdoc controller
+ * @name rfx.controller:RestController
+ * @description
+ * This is a controller using the {@link rfx.rest rest} service.
+ */
+angular.module('rfx')
+  .controller('MainController', ['rest', function (rest) {}]);
